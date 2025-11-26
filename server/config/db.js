d4dca5fa7d32cec9664f521b47e55198884eb916
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-11-26 21:24:33 +0530

    db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,11 +4,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: "password",
-  database: "habit_tracker",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
 });
