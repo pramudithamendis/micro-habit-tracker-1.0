821e81e2177a07d73db70182e41ae0bf8c44e431
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-11-27 10:39:17 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+import axios from "axios";
+
 const API = axios.create({
-  baseURL: "https://micro-habit-tracker-10-production.up.railway.app",
+  baseURL: import.meta.env.PROD ? "https://micro-habit-tracker-10-production.up.railway.app/api" : "/api",
 });
 
 API.interceptors.request.use((config) => {
